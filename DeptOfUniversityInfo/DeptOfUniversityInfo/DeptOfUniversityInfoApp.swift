43afa02d7817b49b9d20fcb79fb9d94724a5d4d7
MatthewCaulder <matt.caulder@gmail.com>
2022-07-27 11:14:09 -0400

    first commit

---
+++ DeptOfUniversityInfo/DeptOfUniversityInfo/DeptOfUniversityInfoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeptOfUniversityInfoApp.swift
+//  DeptOfUniversityInfo
+//
+//  Created by Matt Caulder on 7/27/22.
+//
+
+import SwiftUI
+
+@main
+struct DeptOfUniversityInfoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
